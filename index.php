3b035a010a4cab5e6d927167fdddee84bdd5bca0
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-10 16:32:33 +0100

    Add product cards

--- index.php
+++ index.php
@@ -18,6 +18,21 @@ require_once __DIR__ . "/data/db.php";
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </head>
   <body>
-    
+    <div class="container text-center">
+      <div class="row">
+        <?php foreach($products as $product) { ?>
+          <div class="card" style="width: 18rem;">
+            <img src="<?php echo $product->image ?>" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $product->title ?></h5>
+              <h5><?php echo $product->category ?></h5>
+              <h5><?php echo $product->type ?></h5>
+              <h5><?php echo $product->price ?></h5>
+
+            </div>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
   </body>
 </html>

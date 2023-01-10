6197ea152afee9f1ebccd9fab7a14c1632ebb289
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-10 15:19:00 +0100

    Add category class

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . "/classes/Category.php";
+require_once __DIR__ . "/classes/Product.php";
+require_once __DIR__ . "/classes/Food.php";
+require_once __DIR__ . "/classes/Toy.php";
+
+
+
+
+?>
